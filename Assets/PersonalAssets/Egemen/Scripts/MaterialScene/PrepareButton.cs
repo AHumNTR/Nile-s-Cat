bf6a4c41de78f51a8d1e756b5be61d1d6798246b
Egemen Yerdelen <egemenyerdelen@gmail.com>
2024-10-26 14:11:36 +0300

    Almost Finished Material Scene

---
+++ Assets/PersonalAssets/Egemen/Scripts/MaterialScene/PrepareButton.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrepareButton : MonoBehaviour
+{
+    #region Singleton
+
+    public static PrepareButton Instance;
+        
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+    
+    public int totalDurabilityToChange;
+    
+    [SerializeField] private string sceneToGo;
+    
+    public void OnPrepareButtonPressed()
+    {
+        
+        //GameData.shipCondition = totalDurabilityToChange;
+        Debug.Log(totalDurabilityToChange);
+
+        SceneManager.LoadScene($"{sceneToGo}");
+    }
+}
