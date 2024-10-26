87ec10087fde63a8f4dd7ab0c761e0ec5f419c86
Egemen Yerdelen <egemenyerdelen@gmail.com>
2024-10-26 23:44:20 +0300

    Update Couldron and gameData

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -5,5 +5,13 @@ public static class GameData
     public static Color mixtureColor;
     public static float shipCondition = 0.84f;
     public static int money;
+
     public static int day;
+    public static int WoodCount;
+    public static int CrystalCount;
+    public static int IronCount;
+
+    public static bool IsWoodInCauldron;
+    public static bool IsCrystalInCauldron;
+    public static bool IsIronInCauldron;
 }
