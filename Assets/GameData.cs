7a7a78da9d74a2dc9c16a20fa2527b4fa97b0cdf
Egemen Yerdelen <egemenyerdelen@gmail.com>
2024-10-26 17:55:30 +0300

    MaterialScene Improved

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -6,4 +6,12 @@ public static class GameData
     public static float shipCondition = 0.84f;
     public static int money;
     public static int day;
+    public static int WoodCount;
+    public static int CrystalCount;
+    public static int IronCount;
+
+    public static bool IsWoodInCauldron;
+    public static bool IsCrystalInCauldron;
+    public static bool IsIronInCauldron;
+    
 }
