74d2146bf090c38d4c042449e72fd07443c61e30
Egemen Yerdelen <egemenyerdelen@gmail.com>
2024-10-26 23:31:11 +0300

    CauldronsMiniGame

--- Assets/PersonalAssets/Egemen/Scripts/CauldronScene/Cauldron.cs
+++ Assets/PersonalAssets/Egemen/Scripts/CauldronScene/Cauldron.cs
@@ -1,6 +1,33 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cauldron : MonoBehaviour
 {
+    public bool isSuccess;
+    
+    [SerializeField] private float flamePower;
+    [SerializeField] private float minFlame;
+    [SerializeField] private float maxFlame;
+    
+    private Slider _fireSlider;
+
+    private void Start()
+    {
+        _fireSlider = GetComponentInChildren<Slider>();
+    }
+
+    private void Update()
+    {
+        _fireSlider.value -= flamePower * .8f * Time.deltaTime;
+
+        isSuccess = _fireSlider.value < maxFlame && _fireSlider.value > minFlame;
+    }
+
+    public void OnFireButtonPressed()
+    {
+        _fireSlider.value += flamePower;
+    }
+    
     
 }
