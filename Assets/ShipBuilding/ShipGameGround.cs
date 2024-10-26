7ddd2e5fa53ff01664d334756534eff25fe103e1
Humsi <ahumantr@gmail.com>
2024-10-26 03:23:30 +0300

    added you lose screen

--- Assets/ShipBuilding/ShipGameGround.cs
+++ Assets/ShipBuilding/ShipGameGround.cs
@@ -3,12 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class ShipGameGround : MonoBehaviour
 {
+    public FadeText fadeText;
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.transform.tag!="FirstShipPart"){
+
             GameData.day=0;
             GameData.money=0;
             GameData.shipCondition=0;
-            SceneManager.LoadScene("CustomerScene");
+            fadeText.fadeInText(2);
+            Invoke("loadFirstScene",5);
         }
+    }
+    void loadFirstScene(){
+        SceneManager.LoadScene("CustomerScene");
     }
 }
