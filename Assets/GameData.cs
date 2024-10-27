b1be2f6d5963c2d9fdceb607f9ab9a34872f26ae
Egemen Yerdelen <egemenyerdelen@gmail.com>
2024-10-27 12:08:30 +0300

    SailingScene Fix

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -6,16 +6,16 @@ public static class GameData
     public static float shipCondition = 0.64f;
     public static int money;
 
-    public static int day=2;
+    public static int day;
     public static int WoodCount;
-    public static int WoodDurability = 5;
-    public static int WoodSpeed = 20;
+    public static readonly int WoodDurability = 5;
+    public static readonly int WoodSpeed = 20;
     public static int IronCount;
-    public static int IronDurability = 10;
-    public static int IronSpeed = 5;
+    public static readonly int IronDurability = 10;
+    public static readonly int IronSpeed = 5;
     public static int CrystalCount;
-    public static int CrystalDurability = 20;
-    public static int CrystalSpeed = 10;
+    public static readonly int CrystalDurability = 20;
+    public static readonly int CrystalSpeed = 10;
 
     public static bool IsWoodInCauldron;
     public static bool IsCrystalInCauldron;
